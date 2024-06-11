25734ff4674fd87252611d4af6e8a9a8eb92db4d
iliak <ilyak@sg2d.ru>
2024-06-11 12:43:13 +0300

    initial commit

---
+++ src/calculator/calc.go
@@ -0,0 +1,8 @@
+package calculator
+
+// Version of the calculator
+var Version = "1.0"
+
+func Sum(number1, number2 int) int {
+	return number1 + number2
+}
---
+++ src/calculator/go.mod
@@ -0,0 +1,3 @@
+module github.com/IlyaKB/go.sg2d.ru
+
+go 1.22.4
